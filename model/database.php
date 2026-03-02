2a4a2767d8a527ceb63b31979a93b248e5e5d0ab
ffrenchonionsoupp <160979560+ffrenchonionsoupp@users.noreply.github.com>
2026-03-01 20:13:35 -0500

    Delete model directory
    need to update directory

--- model/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-    function get_db_conn()
-    {
-        //Connect to Database
-        $hostname = "localhost";
-        $dbname = "sdc310L_courseproject";
-        return mysqli_connect($hostname, $dbname);
-    }
-?>
